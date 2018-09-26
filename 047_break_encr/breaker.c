9302a976f5aca6fe321e62add8c9b766738bf3ee
hd97 <hd97@duke.edu>
2018-09-25 23:52:59 -0400

    1.0

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -2,16 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void breaker(FILE * f) {
+int breaker(FILE * f) {
   int c;
   int i;
   int freq[26] = {0};
+  int flag = 0;
   while ((c = fgetc(f)) != EOF) {
     if (isalpha(c)) {
       c = tolower(c);
       i = c - 'a';
       freq[i] += 1;
+      flag = 1;
     }
+  }
+  if (flag == 0) {
+    fprintf(stderr, "NO ALPHA!\n");
+    return 1;
   }
   int max = 0;
   int maxindex = 0;
@@ -27,21 +33,24 @@ void breaker(FILE * f) {
     key += 26;
   }
   fprintf(stdout, "%d\n", key);
+  return 0;
 }
 
 int main(int argc, char ** argv) {
   if (argc != 2) {
-    fprintf(stderr, "Wrong amount of Arguments.");
+    fprintf(stderr, "Wrong amount of Arguments.\n");
     return EXIT_FAILURE;
   }
   FILE * f = fopen(argv[1], "r");
   if (f == NULL) {
-    perror("Could not open file");
+    perror("Could not open file\n");
     return EXIT_FAILURE;
   }
-  breaker(f);
+  if (breaker(f)) {
+    return EXIT_FAILURE;
+  }
   if (fclose(f) != 0) {
-    perror("Failed to close the FILE");
+    perror("Failed to close the FILE\n");
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
