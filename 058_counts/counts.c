c9701f0bab2a123f4d53a4f11dcfed31183cda39
hd97 <hd97@duke.edu>
2018-09-30 17:31:35 -0400

    1.0

--- 058_counts/counts.c
+++ 058_counts/counts.c
@@ -1,17 +1,56 @@
+#include "counts.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "counts.h"
+char * getName(const char * name) {
+  int len = strlen(name);
+  char * myName = malloc((len + 1) * sizeof(*myName));
+  strcpy(myName, name);
+  return myName;
+}
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * myCounts = malloc(sizeof(*myCounts));
+  myCounts->size = 0;
+  myCounts->pointerToArray = NULL;
+  myCounts->unknownTimes = 0;
+  return myCounts;
 }
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  if (name != NULL) {
+    for (int i = 0; i < c->size; i++) {
+      if (!strcmp(c->pointerToArray[i].name, name)) {
+        c->pointerToArray[i].times += 1;
+        return;
+      }
+    }
+    c->size += 1;
+    c->pointerToArray = realloc(c->pointerToArray, c->size * sizeof(*c->pointerToArray));
+    c->pointerToArray[c->size - 1].name = getName(name);
+    c->pointerToArray[c->size - 1].times = 1;
+  }
+  else {
+    c->unknownTimes += 1;
+  }
 }
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  for (int i = 0; i < c->size; i++) {
+    fprintf(outFile, "%s: %d\n", c->pointerToArray[i].name, c->pointerToArray[i].times);
+  }
+  if (c->unknownTimes > 0) {
+    fprintf(outFile, "<unknown>: %d\n", c->unknownTimes);
+  }
+  fclose(outFile);
 }
 
 void freeCounts(counts_t * c) {
   //WRITE ME
+  for (int i = 0; i < c->size; i++) {
+    free(c->pointerToArray[i].name);
+  }
+  free(c->pointerToArray);
+  free(c);
 }
