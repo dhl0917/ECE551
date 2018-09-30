b6cafd364f515635d26103a610abe5ee280e03b3
hd97 <hd97@duke.edu>
2018-09-30 15:52:44 -0400

    1.0

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,16 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  int len = strlen(inputName);
+  char * outputName = malloc((len + 7 + 1) * sizeof(*outputName));
+  for (int i = 0; i < len; i++) {
+    outputName[i] = inputName[i];
+  }
+
+  return strcat(outputName, ".counts\0");
 }
