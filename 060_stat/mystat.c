e9f59eea296a8232c9c1ca6ad4cccb10533ab7d5
hd97 <hd97@duke.edu>
2018-10-09 14:35:39 -0400

    Step 2 Done

--- 060_stat/mystat.c
+++ 060_stat/mystat.c
@@ -31,27 +31,35 @@ int main(int argc, char ** argv) {
   fprintf(stdout, "  File: %s\n", argv[1]);
 
   char * filetype;
+  char access[11];
   switch (mysb.st_mode & S_IFMT) {
     case S_IFBLK:
       filetype = "block special file";
+      access[0] = 'b';
       break;
     case S_IFCHR:
       filetype = "character special file";
+      access[0] = 'c';
       break;
     case S_IFDIR:
       filetype = "directory";
+      access[0] = 'd';
       break;
     case S_IFIFO:
       filetype = "fifo";
+      access[0] = 'p';
       break;
     case S_IFLNK:
       filetype = "symbolic link";
+      access[0] = 'l';
       break;
     case S_IFREG:
       filetype = "regular file";
+      access[0] = '-';
       break;
     case S_IFSOCK:
       filetype = "socket";
+      access[0] = 's';
       break;
     default:
       filetype = "unknown???";
@@ -69,4 +77,68 @@ int main(int argc, char ** argv) {
           mysb.st_dev,
           mysb.st_ino,
           mysb.st_nlink);
+  if (mysb.st_mode & S_IRUSR) {  //2nd
+    access[1] = 'r';
+  }
+  else {
+    access[1] = '-';
+  }
+
+  if (mysb.st_mode & S_IWUSR) {  //3rd
+    access[2] = 'w';
+  }
+  else {
+    access[2] = '-';
+  }
+
+  if (mysb.st_mode & S_IXUSR) {  //4th
+    access[3] = 'x';
+  }
+  else {
+    access[3] = '-';
+  }
+
+  if (mysb.st_mode & S_IRGRP) {  //5th
+    access[4] = 'r';
+  }
+  else {
+    access[4] = '-';
+  }
+
+  if (mysb.st_mode & S_IWGRP) {  //6th
+    access[5] = 'w';
+  }
+  else {
+    access[5] = '-';
+  }
+
+  if (mysb.st_mode & S_IXGRP) {  //7th
+    access[6] = 'x';
+  }
+  else {
+    access[6] = '-';
+  }
+
+  if (mysb.st_mode & S_IROTH) {  //8th
+    access[7] = 'r';
+  }
+  else {
+    access[7] = '-';
+  }
+  if (mysb.st_mode & S_IWOTH) {  //9th
+    access[8] = 'w';
+  }
+  else {
+    access[8] = '-';
+  }
+  if (mysb.st_mode & S_IXOTH) {  //10th
+    access[9] = 'x';
+  }
+  else {
+    access[9] = '-';
+  }
+  access[10] = '\0';
+  fprintf(stdout, "Access: (%04o/%s)\n", mysb.st_mode & ~S_IFMT, access);
+
+  return EXIT_SUCCESS;
 }
