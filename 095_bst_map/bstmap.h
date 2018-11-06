59f189937636cd3ce8233caa7700da83cd2d6508
hd97 <hd97@duke.edu>
2018-11-06 00:15:36 -0500

    1.0

---
+++ 095_bst_map/bstmap.h
@@ -0,0 +1,115 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include "map.h"
+template<typename K, typename V>
+class BstMap : public Map<K, V>
+{
+ private:
+  class Node
+  {
+   public:
+    K key;
+    V value;
+    Node * left;
+    Node * right;
+    Node() : left(NULL), right(NULL) {}
+    Node(K key, V value, Node * left, Node * right) :
+        key(key),
+        value(value),
+        left(left),
+        right(right) {}
+  };
+
+ public:
+  Node * root;
+  BstMap() : root(NULL) {}
+  virtual void add(const K & key, const V & value) {
+    Node ** curr = &root;
+    while (*curr != NULL) {
+      if (key == (*curr)->key) {
+        (*curr)->value = value;
+        return;
+      }
+      else if (key < (*curr)->key) {
+        curr = &((*curr)->left);
+      }
+      else {
+        curr = &((*curr)->right);
+      }
+    }
+    (*curr) = new Node(key, value, NULL, NULL);
+  }
+  virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
+    Node * const * curr = &root;
+    while (*curr != NULL) {
+      if ((*curr)->key == key) {
+        return (*curr)->value;
+      }
+      else if (key < (*curr)->key) {
+        curr = &((*curr)->left);
+      }
+      else {
+        curr = &((*curr)->right);
+      }
+    }
+    throw std::invalid_argument("Find Error!\n");
+  }
+  virtual void remove(const K & key) {
+    Node ** curr = &root;
+    while (*curr != NULL) {
+      if (((*curr)->key) == key) {
+        if ((*curr)->left != NULL && (*curr)->right != NULL) {
+          /*
+	  Node * p = (*curr)->left;
+          while (p->right->right != NULL) {
+            p = p->right;
+          }
+          (*curr)->key = p->key;
+          (*curr)->value = p->value;
+          // delete p;
+	  */
+          Node ** p = &((*curr)->left);
+          while (((*p)->right) != NULL) {
+            p = &((*p)->right);
+          }
+          (*curr)->key = (*p)->key;
+          (*curr)->value = (*p)->value;
+          delete *p;
+          *p = NULL;
+        }
+        else {
+          if ((*curr)->left == NULL) {
+            Node * temp = *curr;
+            (*curr) = (*curr)->right;
+            delete temp;
+          }
+          else if ((*curr)->right == NULL) {
+            Node * temp = *curr;
+            (*curr) = (*curr)->right;
+            delete temp;
+          }
+          else {
+            delete (*curr);
+            *curr = NULL;
+          }
+        }
+      }
+      else if (key < (*curr)->key) {
+        curr = &((*curr)->left);
+      }
+      else {
+        curr = &((*curr)->right);
+      }
+    }
+  }
+  void destroy(Node * current) {
+    if (current != NULL) {
+      destroy(current->left);
+      destroy(current->right);
+      delete current;
+    }
+  }
+  virtual ~BstMap() { destroy(root); }
+  friend void check(void);
+};
---
+++ 095_bst_map/test.cpp
@@ -0,0 +1,38 @@
+#include <assert.h>
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "bstmap.h"
+void check(void) {
+  BstMap<int, int> b;
+  /*
+  b.add(1, 2);
+  b.add(2, 10);
+  b.add(3, 8);
+  assert(b.root->right->right->key == 3 && b.root->right->right->value == 8);
+  */
+
+  assert(b.root != NULL && b.root->key == 1 && b.root->value == 2 && b.root->left == NULL &&
+         b.root->right == NULL);
+  b.add(0, 0);
+  assert(b.root != NULL && b.root->left->key == 0 && b.root->left->value == 0 &&
+         b.root->left->left == NULL && b.root->left->right == NULL);
+  b.add(2, 4);
+  assert(b.root != NULL && b.root->right->key == 2 && b.root->right->value == 4 &&
+         b.root->right->left == NULL && b.root->right->right == NULL);
+  assert(b.lookup(0) == 0);
+  assert(b.lookup(1) == 2);
+  assert(b.lookup(2) == 4);
+  b.add(2, 10);
+  assert(b.lookup(2) == 10);
+  //  b.lookup(3);
+  b.add(3, 8);
+  b.remove(1);
+  assert(b.root->left == NULL && b.root->value == 0 && b.root->key == 0);
+}
+
+int main() {
+  check();
+  return EXIT_SUCCESS;
+}
