bce8721d2d13343a1349004f410df5eecf7a70bb
hd97 <hd97@duke.edu>
2018-11-08 01:21:31 -0500

    101.0

--- 098_dedup/dedup.cpp
+++ 098_dedup/dedup.cpp
@@ -21,7 +21,7 @@ class HashMap
   // double loadFactor;
 
  public:
-  HashMap() : table(new std::vector<std::string>(20)), bucketSize(20), elementNum(0) {}
+  HashMap() : table(new std::vector<std::string>(997)), bucketSize(997), elementNum(0) {}
   double getLoadFactor() { return elementNum / ((1.0) * bucketSize); }
   void rehash() {
     //    std::cout << "need rehash" << std::endl;
@@ -67,6 +67,7 @@ class HashMap
       }
     }
     bucketSize *= 2;
+    // std::cout << "------------------------------------------------------------\n";
     std::swap(table, temp);
     delete temp;
   };
@@ -78,6 +79,9 @@ class HashMap
     // size_t counter = 0;
     // size_t primeArray[10] = {3, 7, 13, 29, 61, 127, 251, 499, 997, 2003};
     s.open(filename);
+
+    // std::cout << getLoadFactor() << std::endl;
+
     if (!s) {
       std::cerr << "Fail to open " << filename << std::endl;
       exit(EXIT_FAILURE);
@@ -92,13 +96,13 @@ class HashMap
     if ((*table)[index].size() == 0) {
       (*table)[index] = filename;
       elementNum += 1;
-      if (getLoadFactor() > 0.8) {
+      if (getLoadFactor() > 0.5) {
         rehash();
       }
     }
     else {
-      std::cout << "#Removing " << filename << " (duplicate of" << std::endl
-                << (*table)[index] << ")." << std::endl
+      std::cout << "#Removing " << filename << " (duplicate of" << (*table)[index] << ")."
+                << std::endl
                 << std::endl;
       std::cout << "rm " << filename << std::endl << std::endl;
     }
@@ -146,12 +150,15 @@ int main(int argc, char ** argv) {
     std::cerr << "Not enough arguments!\n" << std::endl;
     exit(EXIT_FAILURE);
   }
+  // std::hash<std::string> myhash;
+  // size_t x = myhash("bacabccbb");
+  //  size_t y = myhash("cccbccccc");
 
   HashMap myHashMap;
   std::cout << "#!/bin/bash" << std::endl;
   for (int i = 1; i < argc; i++) {
     readDir(argv[i], myHashMap);
   }
-
+  // std::cout << x << "\n" << y << std::endl;
   return EXIT_SUCCESS;
 }
