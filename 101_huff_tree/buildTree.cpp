9ce42c442c6de88483b091e715011b451c1351ff
hd97 <hd97@duke.edu>
2018-11-10 12:44:22 -0500

    1.0

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -1,5 +1,25 @@
-#include "node.h"
+#include <stdio.h>
+#include <string.h>
 
-Node *  buildTree(uint64_t * counts) {
+#include "node.h"
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  int n = 257;
+  for (int i = 0; i < n; i++) {
+    if (counts[i] == 0) {
+      continue;
+    }
+    pq.push(new Node(i, counts[i]));
+  }
+  while (pq.size() > 1) {
+    Node * a = pq.top();
+    pq.pop();
+    Node * b = pq.top();
+    pq.pop();
+    pq.push(new Node(a, b));
+  }
+  Node * ans = pq.top();
+  pq.pop();
+  return ans;
+}
