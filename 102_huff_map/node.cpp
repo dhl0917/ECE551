1d67c241a8e09a5ee1f6bfc8963879ee140a09f2
hd97 <hd97@duke.edu>
2018-11-10 13:29:55 -0500

    Hopefully right

--- 102_huff_map/node.cpp
+++ 102_huff_map/node.cpp
@@ -1,5 +1,16 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
   //WRITE ME!
+  //Base case:
+  if (sym != NO_SYM) {
+    assert(left == NULL);
+    assert(right == NULL);
+    theMap.insert(std::pair<unsigned, BitString>(sym, b));
+    return;
+  }
+  //recursion
+  //to the left
+  (*this).left->buildMap(b.plusZero(), theMap);
+  //to the right
+  (*this).right->buildMap(b.plusOne(), theMap);
 }
-
